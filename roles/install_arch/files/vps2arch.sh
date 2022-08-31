92f47c8bc6ad290279120e6424de0f58054ce666
Harold Cheng <niuchangcun@gmail.com>
2022-09-01 00:01:00 +0800

    vps2arch: track upstream update

--- roles/install_arch/files/vps2arch.sh
+++ roles/install_arch/files/vps2arch.sh
@@ -20,9 +20,9 @@ set -e
 
 # Gathering informations about actual environment.
 if command -v wget >/dev/null 2>&1; then
-	_download() { wget -O- "$@" ; }
+	_download() { wget -U vps2arch -O- "$@" ; }
 elif command -v curl >/dev/null 2>&1; then
-	_download() { curl -fL "$@" ; }
+	_download() { curl -A vps2arch -fL "$@" ; }
 else
 	echo "This script needs curl or wget" >&2
 	exit 2
@@ -41,6 +41,7 @@ get_mirrors() {
 
 cpu_type=$(uname -m)
 
+is_openvz() { [ -d /proc/vz -a ! -d /proc/bc ]; }
 is_lxc() { grep -aqw container=lxc /proc/1/environ ; }
 
 download() {
@@ -54,7 +55,7 @@ download() {
 
 download_and_extract_bootstrap() {
 	local sha1 filename
-	download iso/latest/sha1sums.txt | fgrep "$cpu_type.tar.gz" > "sha1sums.txt"
+	download iso/latest/sha1sums.txt | egrep "[[:digit:]]-$cpu_type.tar.gz" > "sha1sums.txt"
 	read -r sha1 filename < "sha1sums.txt"
 	download "iso/latest/$filename" > "$filename"
 	sha1sum -c sha1sums.txt || exit 1
@@ -79,6 +80,7 @@ download_and_extract_bootstrap() {
 	# FIXME support multiple partitions
 	mount --bind / "/root.$cpu_type/mnt"
 	findmnt /boot >/dev/null && mount --bind /boot "/root.$cpu_type/mnt/boot"
+	findmnt /boot/efi >/dev/null && mount --bind /boot/efi "/root.$cpu_type/mnt/boot/efi"
 	# Workaround for Debian
 	mkdir -p "/root.$cpu_type/run/shm"
 	# Workaround for OpenVZ
@@ -95,9 +97,22 @@ configure_chroot() {
 	for m in $mirrors; do
 		echo 'Server = '"$m"'/$repo/os/$arch'
 	done >> "/root.$cpu_type/etc/pacman.d/mirrorlist"
+	# Install and initialize haveged if needed
+	if ! is_openvz && ! pidof haveged >/dev/null; then
+		# Disable signature check, install and launch haveged and re-enable signature checks.
+		sed -i.bak "s/^[[:space:]]*SigLevel[[:space:]]*=.*$/SigLevel = Never/" "/root.$cpu_type/etc/pacman.conf"
+		chroot_exec 'pacman --needed --noconfirm -Sy haveged && haveged'
+		mv "/root.$cpu_type/etc/pacman.conf.bak" "/root.$cpu_type/etc/pacman.conf"
+	fi
 	chroot_exec 'pacman-key --init && pacman-key --populate archlinux'
+	chroot_exec 'pacman --needed --noconfirm -Sy archlinux-keyring'
 	# Generate fstab
 	chroot_exec 'genfstab /mnt >> /etc/fstab'
+
+	if is_openvz && [ "$kernelver" '<' '2.6.32-042stab111.1' ]; then
+		# Use my repository for OpenVZ-patched systemd
+		sed -i 's;^#\[testing\]$;[tredaelli-systemd]\nServer = http://pkgbuild.com/~tredaelli/repo/systemd/$arch\n\n&;' "/root.$cpu_type/etc/pacman.conf"
+	fi
 }
 
 save_root_pass() {
@@ -120,19 +135,24 @@ delete_all() {
 }
 
 install_packages() {
-	local packages="base linux lvm2 openssh reflector xfsprogs"
+	local packages="base linux lvm2 openssh reflector vim"
 	[ "$bootloader" != "none" ] && packages="$packages $bootloader"
 	# XXX Install gptdisk for syslinux. To be removed then FS#45029 will be closed
 	[ "$bootloader" = "syslinux" ] && packages="$packages gptfdisk"
+	[ -f /sys/firmware/efi/fw_platform_size ] && packages="$packages efibootmgr"
+	[ "$network" = "netctl" ] && packages="$packages netctl"
+	while read -r _ mountpoint filesystem _; do
+		[ "$mountpoint" = "/" -a "$filesystem" = "xfs" ] && packages="$packages xfsprogs"
+	done < /proc/mounts
 	# Black magic!
-	"/root.$cpu_type/usr/lib"/ld-*.so --library-path "/root.$cpu_type/usr/lib" \
+	"/root.$cpu_type/usr/lib"/ld-*.so.2 --library-path "/root.$cpu_type/usr/lib" \
 		"/root.$cpu_type/usr/bin/chroot" "/root.$cpu_type" /usr/bin/pacstrap -M /mnt $packages
 	cp -L "/root.$cpu_type/etc/resolv.conf" /etc
 }
 
 restore_root_pass() {
 	# If the root password is not set, use vps2arch
-	if egrep -q '^root:.?:' "/root.$cpu_type/root.passwd"; then
+	if egrep -q '^root:[^$]' "/root.$cpu_type/root.passwd"; then
 		echo "root:vps2arch" | chpasswd
 	else
 		sed -i '/^root:/d' /etc/shadow
@@ -147,10 +167,13 @@ cleanup() {
 }
 
 configure_bootloader() {
-	local root_dev=$(findmnt -no SOURCE /) root_devs= tmp= needs_lvm2=0
+	local root_dev=$(findmnt -no SOURCE /) root_devs= tmp= needs_lvm2=0 uefi=0
 	case $root_dev in
 	/dev/mapper/*) needs_lvm2=1 ;;
 	esac
+	if [ -f /sys/firmware/efi/fw_platform_size ]; then
+		uefi=$(cat /sys/firmware/efi/fw_platform_size)
+	fi
 
 	if [ $needs_lvm2 -eq 1 ]; then
 		# Some distro doesn't use lvmetad by default
@@ -177,9 +200,16 @@ configure_bootloader() {
 			*)		root_devs="${root_devs:+$root_devs }$tmp"	;;
 			esac
 		done
-		for root_dev in $root_devs; do
-			grub-install --target=i386-pc --recheck --force "$root_dev"
-		done
+		case $uefi in
+		0)
+			for root_dev in $root_devs; do
+				grub-install --target=i386-pc --recheck --force "$root_dev"
+			done
+			;;
+		64)
+			grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+			;;
+		esac
 		grub-mkconfig > /boot/grub/grub.cfg
 	elif [ "$bootloader" = "syslinux" ]; then
 		# If you are still using eth* as interface name, disable "strange" ifnames
@@ -247,6 +277,19 @@ configure_network() {
 }
 
 finalize() {
+	# OpenVZ hacks
+	if is_openvz; then
+		local kernelver
+		read -r _ _ kernelver _ < /proc/version
+		if [ "$kernelver" '>' '3.10' ]; then
+			# Virtuozzo 7 works with systemd, but it needs /etc/resolvconf/resolv.conf.d directory
+			mkdir -p /etc/resolvconf/resolv.conf.d
+		elif [ "$kernelver" '<' '2.6.32-042stab111.1' ]; then
+			# Use my repository for OpenVZ-patched systemd
+			sed -i 's;^#\[testing\]$;[tredaelli-systemd]\nServer = http://pkgbuild.com/~tredaelli/repo/systemd/$arch\n\n&;' /etc/pacman.conf
+		fi
+	fi
+
 	# Enable SSH login for user root (#3)
 	sed -i '/^#PermitRootLogin\s/s/.*/&\nPermitRootLogin yes/' /etc/ssh/sshd_config
 	
@@ -258,17 +301,14 @@ finalize() {
 		
 	EOF
 	
-	reflector -l 70 -p https --sort rate --save /etc/pacman.d/mirrorlist --connection-timeout 20
+	reflector -l 35 -p https --sort rate --save /etc/pacman.d/mirrorlist
 
 	cat <<-EOF
 		Hi,
 		your VM has successfully been reimaged with Arch Linux.
-
 		This script configured $bootloader as bootloader and $network for networking.
-
 		When you are finished with your post-installation, you'll need to reboot the VM the rough way:
 		# sync ; reboot -f
-
 		Then you'll be able to connect to your VM using SSH and to login using your old root password (or "vps2arch" if you didn't have a root password).
 	EOF
 }
@@ -299,14 +339,11 @@ while getopts ":b:m:n:h" opt; do
 	h)
 		cat <<-EOF
 			usage: ${0##*/} [options]
-
 			  Options:
 			    -b (grub|syslinux)           Use the specified bootloader. When this option is omitted, it defaults to grub.
 			    -n (systemd-networkd|netctl) Use the specified networking configuration system. When this option is omitted, it defaults to systemd-networkd.
 			    -m mirror                    Use the provided mirror (you can specify this option more than once).
-
 			    -h                           Print this help message
-
 			    Warning:
 			      On OpenVZ containers the bootloader will be not installed and the networking configuration system will be enforced to netctl.
 		EOF
@@ -327,7 +364,10 @@ shift $((OPTIND - 1))
 [ -z "$mirrors" ] && mirrors=$(get_mirrors)
 
 
-if is_lxc; then
+if is_openvz; then
+	bootloader=none
+	network=netctl
+elif is_lxc; then
 	bootloader=none
 fi
 
